0ed0cc9c99fb3dc63c186c8ef6a2c021dc872ea1
Anton Kudryavtsev <kam123ua@gmail.com>
2019-10-04 22:37:40 +0300

    Add test javalin controller and structure for answer

---
+++ src/main/java/g2/tss/fyre/controllers/CreateController.java
@@ -0,0 +1,13 @@
+package g2.tss.fyre.controllers;
+
+/**
+ * This interface describe creation of interface
+ *
+ * @author Anton Kudryavtsev
+ */
+public interface CreateController {
+    /**
+     * This method create controller
+     */
+    void create();
+}
---
+++ src/main/java/g2/tss/fyre/models/Answer.java
@@ -0,0 +1,45 @@
+package g2.tss.fyre.models;
+
+import com.google.gson.Gson;
+
+/**
+ * This class describe answer model
+ *
+ * @param <T> type of additional info for answer
+ * @author Anton Kudryavtsev
+ */
+public class Answer<T> {
+
+    private boolean status;
+    private T obj;
+
+    /**
+     * Constructor
+     *
+     * @param status status of answer
+     * @param obj    additional info for answer
+     */
+    public Answer(boolean status, T obj) {
+        this.status = status;
+        this.obj = obj;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param status status of answer
+     */
+    public Answer(boolean status) {
+        this.status = status;
+        this.obj = null;
+    }
+
+    /**
+     * Method convert objects to json
+     *
+     * @return answer in gson
+     */
+    public String toJSON() {
+        return new Gson().toJson(this);
+    }
+}
