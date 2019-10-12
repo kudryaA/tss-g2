e592307e40479561090d37776dd962c288f19f5b
DruceLee <andreydrus2@gmail.com>
2019-10-12 21:01:50 +0300

    Добавлено регистрацию модератора. Исправлено javaDoc в классе DataStorage.

---
+++ src/main/java/tss/g2/fyre/models/actions/RegisterModerator.java
@@ -0,0 +1,31 @@
+package tss.g2.fyre.models.actions;
+
+import tss.g2.fyre.models.Answer;
+import tss.g2.fyre.models.datastorage.DataStorage;
+
+public class RegisterModerator implements Action {
+  private DataStorage dataStorage;
+  private String login;
+  private String password;
+  private String name;
+
+  /**
+   * Constructor.
+   *
+   * @param dataStorage data storage object
+   * @param login moderator login
+   * @param password moderator password
+   * @param name moderator name
+   */
+  public RegisterModerator(DataStorage dataStorage, String login, String password, String name) {
+    this.dataStorage = dataStorage;
+    this.login = login;
+    this.password = password;
+    this.name = name;
+  }
+
+  @Override
+  public Answer getAnswer() {
+    return new Answer<>(true, dataStorage.createNewModerator(login, password, name));
+  }
+}
