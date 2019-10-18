fc3e13a01dd8a39b4fed4ddb1324dc8efa9281b7
Anton Kudryavtsev <kam123ua@gmail.com>
2019-10-18 23:00:24 +0300

    Fix random string

--- src/main/java/tss/g2/fyre/utils/RandomString.java
+++ src/main/java/tss/g2/fyre/utils/RandomString.java
@@ -23,8 +23,16 @@ public class RandomString {
    * @return random string
    */
   public String generate() {
-    byte[] array = new byte[length];
-    new Random().nextBytes(array);
-    return new String(array, Charset.forName("UTF-8"));
+    int leftLimit = 97; // letter 'a'
+    int rightLimit = 122; // letter 'z'
+    int targetStringLength = length;
+    Random random = new Random();
+    StringBuilder buffer = new StringBuilder(targetStringLength);
+    for (int i = 0; i < targetStringLength; i++) {
+      int randomLimitedInt = leftLimit + (int)
+          (random.nextFloat() * (rightLimit - leftLimit + 1));
+      buffer.append((char) randomLimitedInt);
+    }
+    return buffer.toString();
   }
 }
