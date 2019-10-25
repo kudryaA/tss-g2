54ec21e94b1cf31562330f521bc7f3de6104e735
Anton Kudryavtsev <kam123ua@gmail.com>
2019-10-25 15:32:38 +0300

    Add api for download image

--- src/main/java/tss/g2/fyre/Application.java
+++ src/main/java/tss/g2/fyre/Application.java
@@ -2,17 +2,17 @@ package tss.g2.fyre;
 
 import io.javalin.Javalin;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Properties;
 
 import tss.g2.fyre.controllers.CreateController;
 import tss.g2.fyre.controllers.javalin.JavalinController;
-import tss.g2.fyre.models.Configuration;
+import tss.g2.fyre.utils.Configuration;
 import tss.g2.fyre.models.datastorage.DataStorage;
 import tss.g2.fyre.models.datastorage.postgress.PostgresDataStorage;
 import tss.g2.fyre.models.entity.Authorization;
-import tss.g2.fyre.utils.ToHash;
 
 /**
  * Main class.
@@ -25,6 +25,7 @@ public class Application {
    * @throws SQLException sql exception
    */
   public static void main(String[] args) throws SQLException {
+    new File("images").mkdirs();
     Properties properties = new Configuration("config/configuration.yml").getProperties();
     int port = Integer.parseInt(properties.getProperty("port"));
     HashMap<String, Authorization> tokenStorage = new HashMap<>();
